1690db5d6e73a3fbe25b306d6e7c693acbbbb054
Jesus Arredondo <jesusarredondo0498@gmail.com>
2024-02-03 14:18:23 -0600

    Creacion de servidor web

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+#Creamos instancia
+
+app = FastAPI()
+
+@app.get('/') #se le agrega la ruta donde desde la web se ingresará
+def get_list():
+    return [1,2,3,]
+
+@app.get('/contact',response_class=HTMLResponse) #endpoint
+def get_list():
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
+
+    """
 
 def run():
     store.get_categories()
